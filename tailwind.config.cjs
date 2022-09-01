54c8a4145e5107bead9f5b121201105731cd6c82
Miguel Magueijo <miguelmagueijo@hotmail.com>
2022-09-01 23:18:03 +0100

    Add button to switch themes
    Has problems with @apply and isn't finished (need optimization)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
@@ -14,14 +15,16 @@ module.exports = {
 			colors: {
 				"primary": "#063F96",
 				"secondary": "#068989",
-				"primary2": "#FE5A1D",
-				"secondary2": "#FFCF07",
+				"primary-a": "rgba(6, 63, 150, 0.75)",
+				"secondary-a": "rgba(6, 137, 137, 0.75)",
+				"darkPrimary": "#FE5A1D",
+				"darkSecondary": "#FFCF07",
+				"darkPrimary-a": "rgba(254, 90, 29, 0.75)",
+				"darkSecondary-a": "rgba(255, 207, 7, 0.75)",
 				"primary3": "#D92122",
 				"secondary3": "#551B8D",
-				"primary2r": "rgba(254, 90, 29, 0.6)",
-				"secondary2r": "rgba(255, 207, 7, 0.6)",
-				"dark": "#000000",
-				"blackMaterial": "#121212",
+				"black": "#121212",
+				"pureBlack": "#000000",
 			},
 			padding: {
 				"0.5": "0.125rem",
