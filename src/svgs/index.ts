e2943d0024e52b7ee858c45f4240e476ead358a7
Miguel Magueijo <miguelmagueijo@hotmail.com>
2022-09-01 23:30:37 +0100

    change folder name

---
+++ src/svgs/index.ts
@@ -0,0 +1,11 @@
+import Briefcase from "./BriefCase.astro";
+import EmbedCode from "./EmbedCode.astro";
+import StudentCap from "./StudentCap.astro";
+import ConceptBulb from "./ConceptBulb.astro";
+
+export default {
+    Briefcase,
+    EmbedCode,
+    StudentCap,
+    ConceptBulb,
+};
