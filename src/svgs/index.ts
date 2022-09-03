cfe200176fe1f0b37ad1db49cc1f6cb1e0615ade
Miguel Magueijo <miguelmagueijo@hotmail.com>
2022-09-03 22:59:44 +0100

    Replace Other div to details tag

--- src/svgs/index.ts
+++ src/svgs/index.ts
@@ -1,9 +1,11 @@
+import ArrowDown from "./ArrowDown.astro";
 import Briefcase from "./BriefCase.astro";
 import EmbedCode from "./EmbedCode.astro";
 import StudentCap from "./StudentCap.astro";
 import ConceptBulb from "./ConceptBulb.astro";
 
 export default {
+    ArrowDown,
     Briefcase,
     EmbedCode,
     StudentCap,
