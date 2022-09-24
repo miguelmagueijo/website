791945789a46c72aa25fabe295855f4d865907f0
Miguel Magueijo <miguelmagueijo@hotmail.com>
2022-09-24 13:10:22 +0100

    Add github icon to projects
    Github icon serves as link to github repo

--- src/svgs/index.ts
+++ src/svgs/index.ts
@@ -1,6 +1,7 @@
 import ArrowDown from "./ArrowDown.astro";
 import Briefcase from "./BriefCase.astro";
 import EmbedCode from "./EmbedCode.astro";
+import Github from "./Github.astro";
 import StudentCap from "./StudentCap.astro";
 import ConceptBulb from "./ConceptBulb.astro";
 
@@ -8,6 +9,7 @@ export default {
     ArrowDown,
     Briefcase,
     EmbedCode,
+    Github,
     StudentCap,
     ConceptBulb,
 };
