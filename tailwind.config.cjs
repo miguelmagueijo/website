5c7cedebd82628d0678ab7e6ecd6bae37e6d793c
kaentos <miguelmagueijo@hotmail.com>
2023-02-27 18:07:55 +0000

    Add tailwind prefix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	prefix: "tw-",
 	darkMode: "class",
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
