ac14d6f107fe8db8f7f24349d91c6ddd1f518e73
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-09-22 21:04:54 +0100

    Update project for tailwind to work

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwindcss(), icon()]
+  integrations: [icon()],
+  vite: { plugins: [tailwindcss()] },
 });
